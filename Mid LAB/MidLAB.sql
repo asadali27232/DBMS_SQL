7074133346b859148128d4c10410c955548f067c
Asad Ali <asadali27232@gmail.com>
2022-11-21 15:30:24 +0500

    Update MidLAB.sql

--- Mid LAB/MidLAB.sql
+++ Mid LAB/MidLAB.sql
@@ -4,12 +4,20 @@ where pub_id = (
 select pub_id from publishers
 where pub_name = 'New Moon Books')
 
---Question 02 (ii)
+--Question 01 (ii)
 select titles.title, au_fname, au_lname from titles
 inner join titleauthor
 on titles.title_id = titleauthor.title_id
 inner join authors
 on authors.au_id = titleauthor.au_id
  
- --Question 02 (iii)
+--Question 01 (iii)
+--What is the name of the employee and his job description who has worked for the
+--publisher who published PC8888?
 
+select employee.fname, employee.lname  from employee
+where pub_id = (
+select pub_id from titles
+where title_id = 'PC8888')
+
+--Question 01 ()
